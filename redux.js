7d0dcb879e14ead6a23729786773c041d2f3235f
mohamed salah <ms.mhmdsalah@gmail.com>
2024-09-09 02:33:39 +0200

    redux thunk

--- redux.js
+++ redux.js
@@ -1,29 +1,46 @@
 console.log(Redux);
 
-
-const addProduct=(product)=>{
+const addProduct = (product) => {
   return {
     type: "products/addProduct",
-    payload:product,
+    payload: product,
   };
-}
+};
 
+const getProducts = (products) => {
+  return {
+    type: "products/getProducts",
+    payload: products,
+  };
+};
 
+const fetchProducts = () => {
+  return async (dispatch) => {
+    const res = await fetch("https://fakestoreapi.com/products");
+    const data = await res.json();
+    console.log(data);
+    dispatch(getProducts(data));
+  };
+};
 
-const productsReducers = (state = [],action)=>{
-  switch(action.type){
+const productsReducers = (state = [], action) => {
+  switch (action.type) {
     case "products/addProduct":
-      if(action.payload.id == 0) return state; 
-      return [...state,action.payload]
-    default :
+      if (action.payload.id == 0) return state;
+      return [...state, action.payload];
+    case "products/getProducts":
+      return [...state,...action.payload]
+    default:
       return state;
   }
-}
+};
 
-const store= Redux.createStore(productsReducers);
+const store = Redux.createStore(
+  productsReducers,
+  Redux.applyMiddleware(ReduxThunk)
+);
 
-
-store.dispatch(addProduct({id:1,title:"product 1"}));
+store.dispatch(addProduct({ id: 1, title: "product 1" }));
 store.dispatch({
   type: "products/addProduct",
   payload: { id: 2, title: "product 2" },
@@ -34,7 +51,6 @@ store.dispatch({
 });
 console.log(store.getState());
 
-
-store.subscribe(()=>{
-  console.log(store.getState())
-})
+store.subscribe(() => {
+  console.log(store.getState());
+});
